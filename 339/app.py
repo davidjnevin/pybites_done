36291c10c7ff586de44d176ca970271a2b088eaa
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2022-11-14 15:41:43 +0100

    Add FastApi 339

---
+++ 339/app.py
@@ -0,0 +1,37 @@
+from typing import Dict, List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Food(BaseModel):
+    """Model from Bite 02"""
+
+    id: int
+    name: str
+    serving_size: str
+    kcal_per_serving: int
+    protein_grams: float
+    fibre_grams: float = 0
+
+
+app = FastAPI()
+foods: Dict[int, Food] = {}
+
+
+@app.post("/", status_code=201)
+async def create_food(food: Food):
+    """Endpoint from Bite 03"""
+    foods[food.id] = food
+    return food
+
+
+# write the two Read endpoints
+@app.get("/", response_model=List[Food])
+async def read_foods():
+    return list(foods.values())
+
+
+@app.get("/{food_id}", response_model=Food)
+async def read_food(food_id: int):
+    return foods[food_id]
